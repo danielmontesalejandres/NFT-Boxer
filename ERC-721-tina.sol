008549cd92fb20699456d272030e1f41ad3a334a
danielmontesalejandres <danielmontesalejandres@gmail.com>
2024-11-07 13:51:45 +0100

    Contrato copiado de OpenZeppelin

---
+++ ERC-721-tina.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact matt_del88@hotmail.com
+contract Constantina is ERC721, ERC721URIStorage, Ownable {
+    constructor(address initialOwner)
+        ERC721("Constantina", "TINA")
+        Ownable(initialOwner)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://sapphire-basic-pigeon-114.mypinata.cloud/ipfs/QmVUh5pVBY9NvczUCUdMJFx6vjhc7A3Qzm5uC7LCM54fGZ";
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
